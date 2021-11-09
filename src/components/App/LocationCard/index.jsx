6c6530a5bd88ec483fed4dc55ab184f09201b228
TannerB <tannerpace@gmail.com>
2021-11-09 11:26:35 -0500

    adds blank locationcard

---
+++ src/components/App/LocationCard/index.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types"
+
+import useStyles from "./styles"
+
+const LocationCard = ({ children }) => {
+  const classes = useStyles()
+
+  return <div>LocationCard</div>
+}
+
+LocationCard.propTypes = {
+  children: PropTypes.node,
+}
+
+export default LocationCard
